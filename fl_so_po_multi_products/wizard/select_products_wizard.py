9941e009641953bc8d7406e2eff8e92734869419
yenademir <github@yenaengineering.nl>
2022-12-26 14:55:52 +0300

    xx

--- fl_so_po_multi_products/wizard/select_products_wizard.py
+++ fl_so_po_multi_products/wizard/select_products_wizard.py
@@ -3,6 +3,17 @@ from datetime import datetime
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo import models, fields, api
 
+def download_attachment(self, cr, uid, ids, context=None):
+    attachment = self.browse(cr, uid, ids[0], context=context)
+    file = attachment.datas
+    file_name = attachment.name
+    file_ext = file_name.split(".")[-1]
+    if file:
+        file = base64.b64decode(file)
+        response = request.make_response(file, [('Content-Type', 'application/octet-stream'), ('Content-Disposition', 'attachment; filename=%s' % file_name)])
+        return response
+    else:
+        raise osv.except_osv(_('Error!'), _('There is no file to download.'))
 
 class SelectProducts(models.TransientModel):
 
@@ -35,3 +46,5 @@ class SelectProducts(models.TransientModel):
                     'display_type': False,
                     'order_id': order_id.id
                 })
+
+        
