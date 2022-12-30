c2e02b9ef2d76b06d1a5f55806da7448ba8b939d
yenademir <github@yenaengineering.nl>
2022-12-30 11:54:03 +0300

    author:matar

--- matar_task/__manifest__.py
+++ matar_task/__manifest__.py
@@ -1,7 +1,9 @@
 {
     'name': 'Task Reporting',
     'description': 'A module for generating task reports based on user',
-    'author': 'matar',
+    'author': 'Matar',
+    'application': True,
+    'installable': True,
     'depends': ['base', 'controllers/task_report_controller'],
     'data': ['views/task_report_view.xml'],
 }
